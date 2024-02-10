e4c24bdd40e69fa1449fc92d6f828408fdff86af
Irina Stepanova <110852608+Irissha@users.noreply.github.com>
2024-02-10 21:22:54 +0500

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ page = requests.get(url)
 print(page.status_code)
 soup = BeautifulSoup(page.text, "html.parser")
 date = soup.findAll("td", {"class": "datatable_cell__LJp3C text-left align-middle overflow-hidden text-v2-black text-ellipsis whitespace-nowrap text-sm font-semibold leading-4 min-w-[106px] left-0 sticky bg-white sm:bg-inherit"})
-money = soup.findAll("td", class_='datatable_cell__LJp3C datatable_cell--align-end__qgxDQ datatable_cell--up__hIuZF')
+money = soup.findAll("td", {"class": "datatable_cell__LJp3C datatable_cell--align-end__qgxDQ datatable_cell--up__hIuZF text-right text-sm font-normal leading-5 align-middle min-w-[77px] rtl:text-right")
 print (money)
 m1 = []
 m2 = []
@@ -25,7 +25,7 @@ m2.reverse()
 print(m2)
 print(m1)
 
-plt.figure(figsize=(40, 20))
+plt.figure(figsize=(60, 60))
 plt.plot(m1, m2, ':o')
 plt.xlabel("Дата")
 plt.ylabel("Доллар в рублях")
